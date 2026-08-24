1a91ac3db53c80b74874ab8e220303730a596db0
¡SomethingSpecial! <78428114+wo-r@users.noreply.github.com>
2026-08-24 14:00:11 -0500

    8/24/26
    made a program in cpp for my favorite things

---
+++ first program/WoodBradenFavorites/WoodBradenFavorites.cpp
@@ -0,0 +1,29 @@
+// WoodBradenFavorites.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// Braden Wood
+// 8/24/2026
+// Favorites V1: Series of cout statements that print to the console.
+
+// Libraries
+#include <iostream>
+
+// Allows to define std without requiring the use of std as a textual form.
+using namespace std;
+
+// Main
+int main()
+{
+    // title
+    cout << "Bradens Favorite Things" << endl << endl; // can use endl or /n (whichever is less annoying)
+
+    // favorite things
+    cout << "Food: Mac N' Cheese" << endl;
+    cout << "Drink: Pepsi Zero Suger" << endl;
+    cout << "Movie: Iron Lung by Markiplier" << endl;
+    cout << "Candy: Trollies (hope that isn't spelt wrong)" << endl;
+    cout << "Color: White" << endl;
+    cout << "Animal: Cats" << endl;
+    cout << "Class: Marching Band" << endl;
+    cout << "Teacher: Dr. Perry" << endl;
+    cout << "Week Day: Saturday" << endl;
+    cout << "Season: Winter" << endl;
+}

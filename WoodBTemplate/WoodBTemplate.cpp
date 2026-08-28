98c171fbf62e009e58df9d4eb5bc757a81436958
¡SomethingSpecial! <78428114+wo-r@users.noreply.github.com>
2026-08-28 11:35:37 -0500

    fix naming schema; Add a template so I dont forget how things work.

---
+++ WoodBTemplate/WoodBTemplate.cpp
@@ -0,0 +1,34 @@
+// WoodBTemplate.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// Braden Wood
+// X/XX/XX
+// [Program Name] V1: description of program.
+
+// libraries
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// declare constant variables
+
+int main()
+{
+    // declare variables
+
+    
+    // Anything between this is generally going to be what the teacher asks for
+    // in class or with homework.
+
+
+    // end process
+    return 0;
+}
+
+/*
+    OTHER NOTES:
+
+    Can use newline abbriviations (\n) or endl
+    ex: cout << "test\n"; or cout << "test" << endl;
+
+    getline(cin, value) & cin.ignore() allow spaced text like "Text With Spaces" to be included in full.
+*/
